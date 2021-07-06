3a9e8d3ddd56f055f53cf60ec52159244779a036
Randal Kimpinski <rkimpins@ualberta.ca>
2021-07-05 22:14:45 -0600

    Added basic game logic and next state generation

---
+++ chopsticks.py
@@ -0,0 +1,103 @@
+FINGERS = 5 # number of fingers per hand
+HANDS = 2 # number of hands per player
+
+
+#TODO add switching rule (1,2 -> 2,1)
+def next_states(state, suicide=False):
+    next_states = []
+
+    if state[0]:
+        hand = (state[3], state[4])
+        opponent_hand = (state[1], state[2])
+    else:
+        hand = (state[1], state[2])
+        opponent_hand = (state[3], state[4])
+
+    hand_total = hand[0] + hand[1]
+
+    # Generate hand switch next states
+    if suicide:
+        start = 0
+    else:
+        start = 1
+
+    new_hands = []
+    for i in range(start, hand_total//2 + 1):
+        if i%5 < (hand_total-i)%5:
+            new_hand = (i % 5, (hand_total - i) % 5 )
+        else:
+            new_hand = ((hand_total - i) % 5, i % 5)
+        if new_hand != hand and new_hand not in new_hands:
+            new_hands.append(new_hand)
+
+    for new_hand in new_hands:
+        if state[0] == 0:
+            new_state = (-state[0]+1, new_hand[0], new_hand[1], state[3], state[4])
+        elif state[0] == 1:
+            new_state = (-state[0]+1, state[1], state[2], new_hand[0], new_hand[1])
+        next_states.append(new_state)
+
+    # Generate hand strike next states
+    temp_opponent_hands = [
+            (opponent_hand[0] + hand[0], opponent_hand[1]),
+            (opponent_hand[0] + hand[1], opponent_hand[1]),
+            (opponent_hand[0], opponent_hand[1] + hand[0]),
+            (opponent_hand[0], opponent_hand[1] + hand[1])
+            ]
+    opponent_hands = []
+    #remove duplicates and remove case where striking hand is dead
+    for new_opponent_hand in temp_opponent_hands:
+        if new_opponent_hand[0]%5 > new_opponent_hand[1]%5:
+            new_opponent_hand = (new_opponent_hand[1]%5, new_opponent_hand[0]%5)
+        else:
+            new_opponent_hand = (new_opponent_hand[0]%5, new_opponent_hand[1]%5)
+        if new_opponent_hand != opponent_hand and new_opponent_hand not in opponent_hands:
+            opponent_hands.append(new_opponent_hand)
+
+    for new_opponent_hand in opponent_hands:
+        if state[0] == 0:
+            new_state = (-state[0]+1, state[1], state[2], new_opponent_hand[0], new_opponent_hand[1])
+        elif state[0] == 1:
+            new_state = (-state[0]+1, new_opponent_hand[0], new_opponent_hand[1], state[3], state[4])
+        next_states.append(new_state)
+
+
+    return next_states
+
+def validate_state(state):
+    #valid turn indicator
+    if state[0] not in [0,1]:
+        return False
+    #valid finger amount
+    for i in range(1, HANDS*2 + 1):
+        if state[i] < 0 or state[i] > FINGERS - 1:
+            return False
+    #at least one player alive
+    alive = False
+    for i in range(1, HANDS*2 + 1):
+        if state[i]:
+            alive = True
+    return alive
+
+def game_over(state):
+    if (state[1] == 0 and state[2] == 0) or (state[3] == 0 and state[4] == 0):
+        return True
+    return False
+
+def winner(state):
+    if (state[1] == 0 and state[2] == 0):
+        return 0
+    if(state[3] == 0 and state[4] == 0):
+        return 1
+    return -1
+
+
+
+def main():
+    test_switch_next_states()
+    #state = (turn, {p1 2 hands}, {p2 2 hands})
+
+
+
+if __name__ == "__main__":
+    main()
